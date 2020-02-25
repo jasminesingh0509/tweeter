22ca1af51df958ba5bb2be6aa229cd0449276636
Jasmine <jasmine_singh1992@hotmail.com>
2020-02-25 22:56:46 +0000

    added counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
-  // --- our code goes here ---
+// Function to update the character counter 
 
-  console.log("hello"); 
+
+// declare max value of count 
+// minus remainder from count to get how many characters 
+$(document).ready(function() {
+  const max = 140;
+  const display = $(".counter");
+  $(".new-tweet")
+    .find("#textArea")
+    .on("keyup", function(event) {
+      const count = $(this).val().length;
+      const remainder = max - count;
+      display.text(remainder);
+
+      if (remainder > 0) {
+        display.css("color", "#545149");
+      } else {
+        display.css("color", "#FF0000");
+      }
+    });
 });
