c6f8ffd9a0cd86207d5c6a0f5c77b113d94d25f7
Jasmine <jasmine_singh1992@hotmail.com>
2020-02-26 22:03:43 +0000

    added ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,91 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+//DATABASE=========================================================
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac"
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants"
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd"
+    },
+    content: {
+      text: "Je pense , donc je suis"
+    },
+    created_at: 1461113959088
+  }
+];
 
+const tweetData = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac"
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants"
+  },
+  created_at: 1461116232227
+};
+
+//FUNCTIONS========================================================
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  // include jQuery previous request
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $(document).ready(function() {
+      $(".container").append($tweet);
+    });
+  }
+};
+
+//function below uses jQuery to add the article and database information to the HTML doc, do not use id
+const createTweetElement = function(tweet) {
+  let $tweet = $(`<article class="tweetContainer">
+  <header>
+    <div>
+      <img src=${tweet.user.avatars}>
+      <p>${tweet.user.name}</p>
+    </div>
+    <span class="handle">${tweet.user.handle}</span>
+  </header>
+  <div class="textAreaTweet">${tweet.content.text}</div>
+  <footer>
+    <div>Date</div>
+  </footer>
+</article>`).addClass("tweet");
+
+  return $tweet;
+};
+
+renderTweets(data);
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    let data = $(this).serialize();
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/tweets/",
+      data
+    }).then(res => console.log(res));
+  });
+});
+
+// $('form').on('submit')
+// let data = $(this).serializer;
+// $.ajax ({ method: 'POST', URL: '/tweets' data: data})
+// .then ()=>
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,7 @@
 // Function to update the character counter 
 
 
-// declare max value of count 
-// minus remainder from count to get how many characters 
+//Function below uses jQuery to access new-tweet and text area, keyup is the event, declare max value of count, minus remainder from count to get how many characters, if remainder is less than 0 display count in red. 
 $(document).ready(function() {
   const max = 140;
   const display = $(".counter");
